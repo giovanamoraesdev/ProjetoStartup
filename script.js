b4f2ca6d2561d6576ff2c7028ed5e5f0b64548c8
Giovana Moraes <GiovanamoraesDev@gmail.com>
2026-08-21 08:43:25 -0300

    feat: adiciona criação de tarefas

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ const taskCard = document.querySelector(".task-card");
 addTaskButton.addEventListener("click", addTask)
 
 function addTask() {
+    const task = prompt("Digite uma nova tarefa:")
+    
     const newTask = document.createElement("p");
 
     newTask.classList.add("task-item");
-    newTask.textContent = "Nova tarefa";
+    
+    newTask.textContent = task;
 
     taskCard.appendChild(newTask);
 
